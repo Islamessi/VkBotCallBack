11c2b7671a1a7831bb147582ad3b3f50597b48f0
islamessi <islamessi@inbox.ru>
2020-10-31 13:31:31 +0300

    2

--- VkBot/Controllers/CallbackController.cs
+++ VkBot/Controllers/CallbackController.cs
@@ -6,7 +6,7 @@ using VkNet.Model;
 using VkNet.Model.RequestParams;
 using VkNet.Utils;
 
-namespace VkBot.Controllers
+namespace Cookie.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
@@ -18,36 +18,39 @@ namespace VkBot.Controllers
         private readonly IConfiguration _configuration;
 
         private readonly IVkApi _vkApi;
-        public CallbackController(IConfiguration configuration)
+
+        public CallbackController(IVkApi vkApi, IConfiguration configuration)
         {
+            _vkApi = vkApi;
             _configuration = configuration;
         }
 
         [HttpPost]
         public IActionResult Callback([FromBody] Updates updates)
         {
-            // Тип события
+            // Проверяем, что находится в поле "type" 
             switch (updates.Type)
             {
-                // Ключ-подтверждение
+                // Если это уведомление для подтверждения адреса
                 case "confirmation":
-                    {
-                        return Ok(_configuration["Config:Confirmation"]);
-                    }
-
-                // Новое сообщение
+                    // Отправляем строку для подтверждения 
+                    return Ok(_configuration["Config:Confirmation"]);
                 case "message_new":
-                    // Десериализация
-                    var msg = Message.FromJson(new VkResponse(updates.Object));
-                    _vkApi.Messages.Send(new MessagesSendParams
                     {
-                        RandomId = new DateTime().Millisecond,
-                        PeerId = msg.PeerId.Value,
-                        Message = msg.Text
-                    });
-                    break;
-            }
+                        // Десериализация
+                        var msg = Message.FromJson(new VkResponse(updates.Object));
 
+                        // Отправим в ответ полученный от пользователя текст
+                        _vkApi.Messages.Send(new MessagesSendParams
+                        {
+                            RandomId = new DateTime().Millisecond,
+                            PeerId = msg.PeerId.Value,
+                            Message = msg.Text
+                        });
+                        break;
+                    }
+            }
+            // Возвращаем "ok" серверу Callback API
             return Ok("ok");
         }
     }
--- VkBot/Startup.cs
+++ VkBot/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using VkNet;
+using VkNet.Abstractions;
+using VkNet.Model;
 
 namespace VkBot
 {
@@ -26,6 +29,11 @@ namespace VkBot
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSingleton<IVkApi>(sp => {
+                var api = new VkApi();
+                api.Authorize(new ApiAuthParams { AccessToken = Configuration["Config:AccessToken"] });
+                return api;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,5 +52,6 @@ namespace VkBot
             app.UseHttpsRedirection();
             app.UseMvc();
         }
+
     }
 }
