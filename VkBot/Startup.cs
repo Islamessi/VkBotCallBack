2fb33d43f23c88b18cbac03ccfff299e79f771c4
islamessi <islamessi@inbox.ru>
2021-06-06 22:02:27 +0300

    1

--- VkBot/Startup.cs
+++ VkBot/Startup.cs
@@ -20,6 +20,7 @@ namespace VkBot
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
+
         {
             services.AddSingleton<IVkApi>(sp => {
                 var api = new VkApi();
