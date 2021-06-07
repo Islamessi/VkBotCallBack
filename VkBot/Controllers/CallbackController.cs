eaa07a2240f60799e00c95e61582489bc0e30e63
islamessi <islamessi@inbox.ru>
2021-06-07 19:17:10 +0300

    1q

--- VkBot/Controllers/CallbackController.cs
+++ VkBot/Controllers/CallbackController.cs
@@ -46,6 +46,7 @@ namespace Cookie.Controllers
                 case "message_new":
                     //Spredsheet.ReadEntriesMas2();
                     // Десериализация
+                    SendMessage("aaa", 266006795);
                     var msg = Message.FromJson(new VkResponse(updates.Object));
                     //var tmp = DateTime.Now.Hour;
                     //if (tmp >= 0 && tmp <= 3)
