3a61d2f2ee24701da735de63686a2ebf0009230d
islamessi <islamessi@inbox.ru>
2021-06-10 00:10:00 +0300

    1

--- VkBot/Methods.cs
+++ VkBot/Methods.cs
@@ -224,30 +224,30 @@ namespace VkBot
                             Program.UsersInfo[Program.UsersInfo.Count - 1].Add(2);
                         }
                         break;
-                    case "игра \"пенальти\"":
-                        using (var db = new MyContext())
-                        {
-                            var user = db.Users.Where(p => p.VkId == peerID);
-                            if (user.Count() == 0)
-                            {
-                                var users = CallbackController._vkApi.Users.Get(new long[] { (long)peerID }).FirstOrDefault();
-                                User user1 = new User { VkId = peerID, FirstName = users.FirstName, LastName = users.LastName };
-                                db.Users.Add(user1);
-                                db.SaveChanges();
-                            }
-                        }
-                        Program.UsersInfo.Add(new List<long?> { peerID });
-                        Program.UsersInfo[Program.UsersInfo.Count - 1].Add(10);
-                        Program.UsersInfo[Program.UsersInfo.Count - 1].Add(0);
-                        Program.UsersInfo[Program.UsersInfo.Count - 1].Add(0);
-                        Program.UsersInfo[Program.UsersInfo.Count - 1].Add(0);
-                        CallbackController.SendMessage("Цель данной игры, забить больше по пенальти и победить)) " +
-                            "Вы первым стоите на воротах, перед вами 9 кнопок. Вы выбираете, ту, " +
-                            "куда прыгаете. Бот рандомно выбирает куда бить, если вы попали в ту же область, " +
-                            "вы ловите мяч. После 5 ударов вы бьете 5 раз, а бот становится на ворота. В случае победы вы получите " +
-                            "все голы, которые смогли забить) Удачи!)\n\n Выберите сложность игры. Если вы победите, то кол-во забитых " +
-                            "голов умножится на этот коэффициент!", peerID, Keyboards.LevelKeyboard);
-                        break;
+                    //case "игра \"пенальти\"":
+                    //    using (var db = new MyContext())
+                    //    {
+                    //        var user = db.Users.Where(p => p.VkId == peerID);
+                    //        if (user.Count() == 0)
+                    //        {
+                    //            var users = CallbackController._vkApi.Users.Get(new long[] { (long)peerID }).FirstOrDefault();
+                    //            User user1 = new User { VkId = peerID, FirstName = users.FirstName, LastName = users.LastName };
+                    //            db.Users.Add(user1);
+                    //            db.SaveChanges();
+                    //        }
+                    //    }
+                    //    Program.UsersInfo.Add(new List<long?> { peerID });
+                    //    Program.UsersInfo[Program.UsersInfo.Count - 1].Add(10);
+                    //    Program.UsersInfo[Program.UsersInfo.Count - 1].Add(0);
+                    //    Program.UsersInfo[Program.UsersInfo.Count - 1].Add(0);
+                    //    Program.UsersInfo[Program.UsersInfo.Count - 1].Add(0);
+                    //    CallbackController.SendMessage("Цель данной игры, забить больше по пенальти и победить)) " +
+                    //        "Вы первым стоите на воротах, перед вами 9 кнопок. Вы выбираете, ту, " +
+                    //        "куда прыгаете. Бот рандомно выбирает куда бить, если вы попали в ту же область, " +
+                    //        "вы ловите мяч. После 5 ударов вы бьете 5 раз, а бот становится на ворота. В случае победы вы получите " +
+                    //        "все голы, которые смогли забить) Удачи!)\n\n Выберите сложность игры. Если вы победите, то кол-во забитых " +
+                    //        "голов умножится на этот коэффициент!", peerID, Keyboards.LevelKeyboard);
+                    //    break;
                     case "пинг":
                         CallbackController.SendMessage("Понг", peerID);
                         break;
