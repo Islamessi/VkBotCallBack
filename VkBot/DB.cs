e9c8eaf1868c2ca3e7303bf994f522799ebe769b
islamessi <islamessi@inbox.ru>
2022-05-05 20:07:57 +0300

    cookie

--- VkBot/DB.cs
+++ VkBot/DB.cs
@@ -38,6 +38,7 @@ namespace VkBot
         public long? Score { get; set; } = 0;
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Last2Name { get; set; }
         public DateTime DatePredskazanie { get; set; }
     }
 
@@ -57,7 +58,7 @@ namespace VkBot
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder
-            .UseSqlite(@"Data Source=GameDB.db;");
+            .UseSqlite(@"Data Source=GameDB2.db;");
         }
     }
 }
