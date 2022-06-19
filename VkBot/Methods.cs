cb797087f32656d090c490feff4474376fdc77e3
islamessi <islamessi@inbox.ru>
2022-06-19 21:26:47 +0300

    1

--- VkBot/Methods.cs
+++ VkBot/Methods.cs
@@ -626,6 +626,7 @@ namespace VkBot
                             break;
                         case "тест по химии":
                         case "тест2":
+                        case "тест3":
                             CallbackController.SendMessage("Учти, что тест надо проходить сразу, иначе я " +
                                 "аннулирую твою попытку). Если ответов несколько пиши их в порядке возрастания " +
                                 "без разделителей (123). \n" +
@@ -652,7 +653,11 @@ namespace VkBot
                             else if (userMessage == "тест2")
                             {
                                 SendQestUser(16, 17, peerID);
-                            }    
+                            }
+                            else if (userMessage == "тест3")
+                            {
+                                SendQestUser(18, 19, peerID);
+                            }
 
                             break;
                         case "пинок":
