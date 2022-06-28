9a9352046a149843ef7d7197121c6cc32a073bfd
islamessi <islamessi@inbox.ru>
2022-06-29 00:55:49 +0300

    1

--- VkBot/Keyboards.cs
+++ VkBot/Keyboards.cs
@@ -26,15 +26,17 @@ namespace VkBot
                         shuffled.RemoveAt(i);
                     }
                 }
-                for (int i = 0; i < shuffled.Count; i++)
-                {
-                    if (shuffled[i].Length > 14)
-                    {
-                        var tmp = shuffled[i];
-                        shuffled.RemoveAt(i);
-                        shuffled.Add(tmp);
-                    }
-                }
+                //for (int i = 0; i < shuffled.Count; i++)
+                //{
+                    
+                //    if (shuffled[i].Length > 14)
+                //    {
+                //        var tmp = shuffled[i];
+                //        shuffled.RemoveAt(i);
+                //        shuffled.Add(tmp);
+                //    }
+                //}
+                shuffled.OrderBy(p => p.Length);
                 for (int i = 0; i < shuffled.Count - 1; i++)
                 {
                     userKey.AddButton(shuffled[i], "", KeyboardButtonColor.Primary, "");
